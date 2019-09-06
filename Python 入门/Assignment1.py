ce976960db76805bb5e8f7b74844162fea438121
xueqileng <53203949+xueqileng@users.noreply.github.com>
2019-09-06 11:25:36 +0800

    Add files via upload

---
+++ Python 入门/Assignment1.py
@@ -0,0 +1,12 @@
+# Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output.
+
+#---------DesiredOutput---------
+#hello world
+
+#-----------SourceCode----------
+# The code below almost works
+
+#print("hello world")
+
+#-------------Answer------------
+print("hello world")
---
+++ Python 入门/Assignment2-3.py
@@ -0,0 +1,21 @@
+#Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
+#Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). 
+#You should use input to read a string and float() to convert the string to a number. 
+#Do not worry about error checking or bad user data.
+
+#---------DesiredOutput---------
+#Pay: 96.25
+
+#-----------SourceCode----------
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+
+#-------------Answer------------
+hrs = input("Enter Hours:")
+hrs = float(hrs)
+rate = input("Enter Rate:")
+rate = float(rate)
+pay = hrs * rate
+pay = str(pay)
+print("Pay: " + pay)
